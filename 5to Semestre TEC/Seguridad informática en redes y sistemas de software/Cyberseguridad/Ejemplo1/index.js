701a67d34015cfb1c8621cafca07c8c4a493989a
Emilio Sibaja <A01025139@itesm.mx>
2022-09-01 15:42:55 -0500

    Com

--- 5to Semestre TEC/Seguridad informática en redes y sistemas de software/Cyberseguridad/Ejemplo1/index.js
+++ 5to Semestre TEC/Seguridad informática en redes y sistemas de software/Cyberseguridad/Ejemplo1/index.js
@@ -1,46 +1,67 @@
-const bodyParser = require("body-parser");
-const express = require("express");
-const fs = require("fs")
+const express = require("express")
+const fs=require("fs")
+const bodyParser=require("body-parser")
 const multer = require("multer")
+const crypto=require("crypto")
+const {MongoClient} = require("mongodb")
 
 
-const app = express();
-app.use(bodyParser.urlencoded({extened:true}));
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}))
 app.set("view-engine", "ejs");
 const uploads=multer({dest: ".temp"})
 
-app.get("/descargar", (req, res) => {
-    let files = fs.readdirSync(__dirname+"/.storage")
+let db;
+
+function connectionToDb(){
+    let client = new MongoClient("mongodb://localhost/")
+    client.connect();
+    console.log("Conectado a la base de datos")
+    db = client.db();
+}
+
+
+app.get("/descargar", (req, res)=>{
+    let files=fs.readdirSync(__dirname+"/.storage");
     res.render("descargar.ejs", {archivos:files})
 })
 
-
-app.post("/descargar", (req, res) => {
-    res.download(__dirname+"/.storage"+req.body.documentos, (err)=>{
-        if (err) throw err;
+app.post("/descargar",(req, res)=>{
+    let temporal=__dirname+"/.temp/"+req.body.documentos;
+    let inputFS=fs.createReadStream(__dirname+"/.storage/"+req.body.documentos)
+    let outputFS=fs.createWriteStream(__dirname+"/.temp/"+req.body.documentos)
+    let key="abcabcabcabcabcabcabcabcabcabc12"
+    let iv="abcabcabcabcabc1"
+    let cipher=crypto.createDecipheriv("aes-256-cbc", key, iv)
+    inputFS.pipe(cipher).pipe(outputFS)
+    outputFS.on("finish",()=>{
+        res.download(temporal, (err)=>{
+            if (err) throw err;
+            fs.unlinkSync(temporal);
+        })
     })
+})
 
+app.get("/cargar", (req, res)=>{
+    res.render("cargar.ejs")
 })
 
 
-app.get("/cargar", (req, res) => {
+app.post("/cargar", uploads.single("archivo"), (req, res)=>{
+    let rutaDefinitiva="/.storage/"+req.body.nombre
     let inputFS=fs.createReadStream(__dirname+"/.temp/"+req.file.filename)
     let outputFS=fs.createWriteStream(__dirname+"/.storage/"+req.body.nombre)
-    let key = "abcabcabcabcabcabcabcabcabc12"
-    let iv = "abcabcabacabc1"
-    let cipher=crypto.createCipheriv("aes")
-
-    res.render("cargar.ejs");
+    let key="abcabcabcabcabcabcabcabcabcabc12"
+    let iv="abcabcabcabcabc1"
+    let cipher=crypto.createCipheriv("aes-256-cbc", key, iv)
+    inputFS.pipe(cipher).pipe(outputFS)
+    outputFS.on("finish",()=>{
+        fs.unlinkSync(__dirname+"/.temp/"+req.file.filename)
+    })
+    res.render("cargar.ejs")
 
 })
-
-app.post("/cargar", uploads.single("archivo"), (req, res) =>{
-    console.log("xd");
-})
-
 app.listen(1337, ()=>{
-
-    console.log("Server started on port 1337...")
-
-
+    connectionToDb();
+    console.log("Server started on port 1337....")
 })
